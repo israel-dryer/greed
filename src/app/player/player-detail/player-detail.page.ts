5c2637807b6d637fe9b2347201c6846a45d13f8e
Israel Dryer <israel.dryer@appforia.net>
2025-02-11 13:19:01 -0500

    implement player detail page

--- src/app/player/player-detail/player-detail.page.ts
+++ src/app/player/player-detail/player-detail.page.ts
@@ -2,18 +2,18 @@ import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {
-  AlertController,
-  IonButton,
+  AlertController, IonBackButton,
+  IonButton, IonButtons,
   IonContent,
   IonHeader,
-  IonIcon,
+  IonIcon, IonInput, IonItem, IonLabel, IonList,
   IonTitle,
   IonToolbar
 } from '@ionic/angular/standalone';
 import {PlayerService} from "../player.service";
 import {Player} from "../../shared/types";
 import {addIcons} from "ionicons";
-import {bookmark, bookmarkOutline} from "ionicons/icons";
+import {bookmark, bookmarkOutline, pencil, trash} from "ionicons/icons";
 import {Router} from "@angular/router";
 
 @Component({
@@ -21,7 +21,7 @@ import {Router} from "@angular/router";
   templateUrl: './player-detail.page.html',
   styleUrls: ['./player-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon, IonButtons, IonBackButton, IonList, IonItem, IonLabel, IonInput]
 })
 export class PlayerDetailPage implements OnInit {
 
@@ -30,9 +30,45 @@ export class PlayerDetailPage implements OnInit {
   readonly playerService = inject(PlayerService);
   player?: Player
 
+  // statistics
+  lastPlayed = '';
+  hoursPlayed = '';
+  totalRolls = '';
+  gamesPlayed = '';
+  gamesWon = '';
+  winRate = '';
+  longestWinStreak = '';
+  fastestWin = '';
+  robberRate = '';
+
+  constructor() {
+    addIcons({pencil, bookmark, bookmarkOutline, trash});
+  }
+
   ngOnInit(): void {
     addIcons({bookmark, bookmarkOutline})
     this.player = this.playerService.getActivePlayer();
+    this.formatStatistics();
+  }
+
+  formatStatistics() {
+    if (this.player) {
+      this.lastPlayed = this.player.lastPlayed === 0 ? 'Never' : new Date(this.player.lastPlayed).toLocaleString();
+      this.hoursPlayed = ((this.player.secondsPlayed / 60) / 60).toFixed(1) + ' hours';
+      this.totalRolls = this.player.totalRolls.toFixed(0);
+      this.gamesPlayed = this.player.gamesPlayed.toFixed(0);
+      this.gamesWon = this.player.gamesWon.toFixed(0);
+      this.winRate = (this.player.gamesPlayed === 0 ? '0%' : (this.player.gamesWon / this.player.gamesPlayed).toLocaleString(undefined, {
+        style: 'percent',
+        maximumFractionDigits: 1
+      }));
+      this.longestWinStreak = this.player.longestWinsStreak.toFixed(0);
+      this.fastestWin = (this.player.secondsPlayed / 60).toFixed(1) + ' minutes';
+      this.robberRate = (this.player.totalRolls === 0 ? '0%' : (this.player.robberRolls / this.player.totalRolls).toLocaleString(undefined, {
+        style: 'percent',
+        maximumFractionDigits: 1
+      }));
+    }
   }
 
   async showEditPlayerNameDialog() {
@@ -41,7 +77,7 @@ export class PlayerDetailPage implements OnInit {
     const alert = await this.alertController.create({
       header: 'Edit Player Name',
       inputs: [{label: 'Name', type: 'text', name: 'playerName', value: this.player.name}],
-      buttons: [{text: 'Submit', role: 'submit'}]
+      buttons: [{text: 'Cancel', role: 'cancel'}, {text: 'Submit', role: 'submit'}]
     });
     alert.onDidDismiss().then(async event => {
       if (event.role !== 'submit') {
@@ -62,11 +98,11 @@ export class PlayerDetailPage implements OnInit {
     let message = 'Bookmarking a player allows you to use the Personal Stats link in the Settings menu.'
     this.player.isUser === 1
       ? message += '\n\n Remove this bookmark?'
-      : message += '\n\n Add bookmar to this player?'
+      : message += '\n\n Add bookmark to this player?'
     const alert = await this.alertController.create({
       header: 'Bookmark Player',
       message,
-      buttons: [{text: 'Submit', role: 'submit'}]
+      buttons: [{text: 'Cancel', role: 'cancel'},{text: 'Submit', role: 'submit'}]
     });
     alert.onDidDismiss().then(async event => {
       if (event.role !== 'submit') {

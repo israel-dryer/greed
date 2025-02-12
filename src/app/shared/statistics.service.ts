d1913ccad5f2b86818ef6dce8da4620aa4c41bec
Israel Dryer <israel.dryer@appforia.net>
2025-02-11 20:23:42 -0500

    add function to calculate player stats

--- src/app/shared/statistics.service.ts
+++ src/app/shared/statistics.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {db} from './database';
+import {createHistogram} from "./utilities";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatisticsService {
 
-  constructor() { }
+
+  async updatePlayerStatsById(id: number) {
+
+    const histogram = createHistogram();
+    const rolls = await db.rolls.where({playerId: id}).toArray();
+    const totalRolls = rolls.length;
+    const robberRolls = rolls.filter(roll => roll.isRobber).length;
+    for (const roll of rolls) {
+      histogram[roll.total] += 1;
+    }
+
+    const gameList = await db.games
+      .filter(x => x.roster.filter(x => x.id === id).length > 0)
+      .toArray();
+
+    const gamesPlayed = gameList.length;
+
+    let fastestWinSeconds = 0;
+    let gamesWon = 0;
+    gameList.filter(x => x.winnerId === id).forEach(game => {
+      gamesWon++;
+      if (fastestWinSeconds === 0) {
+        fastestWinSeconds = game.duration;
+      } else if (fastestWinSeconds > game.duration) {
+        fastestWinSeconds = game.duration;
+      }
+    });
+    const secondsPlayed = gameList.filter(x => x.completedOn).map(x => x.duration).reduce((a, b) => a + b);
+    let lastPlayed = gameList.at(0)?.createdOn;
+
+    // win streak calculation
+    let currentStreak = 0;
+    let longestWinsStreak = 0;
+    gameList.forEach(game => {
+      if (game.winnerId !== id) {
+        if (longestWinsStreak < currentStreak) {
+          longestWinsStreak = currentStreak;
+        }
+        currentStreak = 0;
+      } else {
+        currentStreak++;
+      }
+    });
+    if (longestWinsStreak < currentStreak) {
+      longestWinsStreak = currentStreak;
+    }
+
+    const changes = {histogram, lastPlayed, gamesPlayed, gamesWon, secondsPlayed, robberRolls, totalRolls, fastestWinSeconds, longestWinsStreak};
+    await db.players.update(id, changes);
+  }
+
+
 }

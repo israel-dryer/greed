52d75cd7ec9490c068f91373ec542270b83d58ab
Israel Dryer <israel.dryer@appforia.net>
2025-02-23 01:23:48 -0500

    update icon assets for red theme

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,14 +4,14 @@ const config: CapacitorConfig = {
   appId: 'net.appforia.settlersdice',
   appName: 'Settlers Dice',
   webDir: 'www',
-  backgroundColor: '#285021',
+  backgroundColor: '#782b2f',
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
       launchAutoHide: true,
       androidSplashResourceName: 'splash',
       androidScaleType: 'CENTER_CROP',
-      backgroundColor: '#285021',
+      backgroundColor: '#782b2f',
       splashFullScreen: false,
       splashImmersive: false,
     }

1016ecb64ba1790a7dbca8e6699c001ae898cb25
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 08:48:05 -0500

    bump version upon deployment.

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 // Service worker for PWA
-const CACHE_NAME = 'catan-dice-v0.0.1';
+const CACHE_NAME = 'catan-dice-v0.0.7';
 
 self.addEventListener('install', (event) => {
   // Skip waiting to activate new service worker immediately

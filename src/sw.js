d28fa437ed061002884cfbe309c9dda185888482
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 08:54:20 -0500

    add maskable icon

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 // Service worker for PWA
-const CACHE_NAME = 'catan-dice-v0.0.7';
+const CACHE_NAME = 'catan-dice-v0.0.8';
 
 self.addEventListener('install', (event) => {
   // Skip waiting to activate new service worker immediately

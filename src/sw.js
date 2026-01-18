b75793a960eac8f2a84436a1d4e9b979792d6cae
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 09:30:37 -0500

    update splash screen theme color

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 // Service worker for PWA
-const CACHE_NAME = 'catan-dice-v0.0.11';
+const CACHE_NAME = 'catan-dice-v0.0.13';
 
 self.addEventListener('install', (event) => {
   // Skip waiting to activate new service worker immediately
